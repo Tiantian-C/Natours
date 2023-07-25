2c48009b869d97540a12c2568f5b1526807ac8a0
陈梦恬 <mengtianchen@s-MacBook-Air.lan>
2023-07-24 20:47:17 -0700

    108

---
+++ .history/models/tourModel_20230724190108.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose');
+
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'a tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+//virtual property
+tourSchema.virtual('durationWeeks').get(function () {
+    return this.duration / 7;
+})
+
+//DOCUMENT MIDDLEWARE:runs be
+tourSchema.pre('save', function () {
+    
+})
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
