b2c957b621a42c37da389628da6271506843451f
陈梦恬 <mengtianchen@s-MacBook-Air.lan>
2023-07-27 23:26:18 -0700

    131

---
+++ .history/controllers/userController_20230727212811.js
@@ -0,0 +1,43 @@
+
+const  = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
+
+exports.getAllUsers = (req, res) => {
+  const tours = await features.query;
+  //SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    },
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This roUte is not yet defined',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This roUte is not yet defined',
+  });
+};
---
+++ .history/models/userModel_20230727221045.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please tell us your name!']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email']
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 8,
+        select:false
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password!'],
+        validate: {
+            // this only works on SAVE and CREATE
+            validator: function (el) {
+                return el === this.password;
+            },
+            message:'Passwords are not the same'
+        }
+    }
+    
+});
+
+//excute between get the data and persist to the database
+userSchema.pre('save', async function (next) {
+    //this refers to the current document
+    //Only run this function if password was actually modified
+    if (!this.isModified('password')) return next();
+    
+    //Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+    
+    //Delete passwordConfirm field
+    //we do not want passwordConfirm to persist in database
+    this.passwordConfirm = undefined;
+    next();
+})
+
+userSchema.method.correctPassword = function(candidat)
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
