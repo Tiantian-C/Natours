63a9ac8aae6c800d7c116c695089b5087b806db4
陈梦恬 <mengtianchen@s-MacBook-Air.lan>
2023-07-29 23:08:48 -0700

    146

---
+++ .history/app_20230729223432.js
@@ -0,0 +1,73 @@
+
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+//1) GLOBAL MIDDLEWARES
+//it will be executed for every incoming request, regardless of the route
+
+
+//SET security HTTP headers
+app.use(helmet());
+
+//Developmeng logging
+if (process.env.NODE_ENV === 'developmet') {
+    app.use(morgan('dev'));
+}
+
+//Limit requests from same API
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests fron this IP,please try again in an hour!'
+});
+
+//Test middleware
+app.use('/api', limiter);
+
+
+//Body parser,reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+
+//Data sanitizatio
+
+//Serving static files
+app.use(express.static(`${__dirname}/public`))
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    //console.log(req.headers);
+    next();
+})
+
+//3)ROUTES
+   
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+
+    
+    // const err = new Error(`Can not find ${req.originalUrl} on this server!`);
+    // err.status = 'fail';
+    // err.statusCode = 404;
+    //next(err);
+
+    next(new AppError(`Can not find ${req.originalUrl} on this server!`,404));
+})
+
+app.use(globalErrorHandler);
+
+
+
+module.exports = app;
+
