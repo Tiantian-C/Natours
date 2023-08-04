dfeaedb0669d710c14e915414d9352dfe2fa3f9f
陈梦恬 <mengtianchen@s-MacBook-Air.lan>
2023-08-04 02:35:06 -0700

    185

---
+++ .history/controllers/viewsController_20230803234258.js
@@ -0,0 +1,22 @@
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //1) Get Tour data from collection
+    const tours = await Tour.find();
+  //2)Build Template
+
+  //3)Render that template using tour data from 1)
+  res.status(200).render('overview', {
+      title: 'All Tours',
+      tours
+  });
+});
+
+exports.getTour = (req, res) => {
+  //1) get the data,for the requested tour() 
+
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  });
+};
